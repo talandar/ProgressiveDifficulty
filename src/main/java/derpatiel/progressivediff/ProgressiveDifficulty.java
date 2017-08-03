a600076d0892e5e2e76d286ba56a2a8bab4bea1a
Talandar <jmtalandar@gmail.com>
2017-08-03 07:36:13 -0400

    move to 2.0.1 and remove really spammy log messages

--- src/main/java/derpatiel/progressivediff/ProgressiveDifficulty.java
+++ src/main/java/derpatiel/progressivediff/ProgressiveDifficulty.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class ProgressiveDifficulty
 {
     public static final String MODID = "progressivedifficulty";
-    public static final String VERSION = "1.12_2.0";
+    public static final String VERSION = "1.12_2.0.1";
 
     public static boolean oldConfigExists = false;
 
