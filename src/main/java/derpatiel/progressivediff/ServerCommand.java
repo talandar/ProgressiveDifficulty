234d9b4f602a44ebebd7006a865339791882da8d
Talandar <jmtalandar@gmail.com>
2017-08-27 11:15:48 -0400

    new command - 'simulate' that simulates a spawn for better difficulty debugging

--- src/main/java/derpatiel/progressivediff/ServerCommand.java
+++ src/main/java/derpatiel/progressivediff/ServerCommand.java
@@ -1,25 +1,26 @@
 package derpatiel.progressivediff;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import derpatiel.progressivediff.util.LOG;
 import derpatiel.progressivediff.util.MobNBTHandler;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.registry.EntityEntry;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
-
-import java.util.Iterator;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import javax.annotation.Nullable;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by Jim on 5/7/2017.
@@ -34,7 +35,11 @@ public class ServerCommand extends CommandBase {
             "    same dimension as the player",
             "\"progdiff killmobs\" kill all mobs in the ",
             "    same dimension as the player",
-            "\"progdiff region\" print the region the player is currently in"
+            "\"progdiff region\" print the region the player is currently in",
+            "\"progdiff simulate [mobId]\" simulates the spawn of the given",
+            "    mob at the player's location and returns the difficulty",
+            "\"progdiff simulateDetailed [mobId]\" as with simulate, but",
+            "    prints much more detailed results"
     };
 
     @Override
@@ -44,16 +49,62 @@ public class ServerCommand extends CommandBase {
 
     @Override
     public String getUsage(ICommandSender sender) {
-        return "\"progdiff [sync|killmodified|killmobs|region]\"";
+        return "\"progdiff [sync|killmodified|killmobs|region|simulate|simulateDetailed]\"";
     }
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        if(args.length==0 || args.length>1){
-            sendChat(sender,usage);
-        }else if(args[0].equalsIgnoreCase("sync")){
+        if (args.length == 0 || args.length > 2) {
+            sendChat(sender, usage);
+        } else if (args[0].equalsIgnoreCase("sync")) {
             DifficultyManager.syncConfig();
             sendChat(sender, new String[]{"Synced config."});
+        }else if(args[0].equalsIgnoreCase("simulate") || args[0].equalsIgnoreCase("simulateDetailed")){
+            boolean detailed = args[0].equalsIgnoreCase("simulateDetailed");
+            if(args.length<2){
+                sendChat(sender, new String[]{"No mob provided.  Call this command with a mob to simulate. (try tab completion!)"});
+                return;
+            }
+            if(args.length>2){
+                sendChat(sender, new String[]{"Too many arguments to simulate/simulateDetailed."});
+                return;
+            }
+            String mobType = args[1];
+            Region senderRegion = DifficultyManager.getRegionForPosition(sender.getEntityWorld().provider.getDimension(),sender.getPosition());
+            List<EntityEntry> matchingMob = ForgeRegistries.ENTITIES.getValues().stream()
+                    .filter(entry->entry.getName().equalsIgnoreCase(mobType))
+                    .collect(Collectors.toList());
+            if(matchingMob.size()!=1){
+                sendChat(sender, new String[]{"No matching mob found for "+mobType+".  Try using tab completion to find a mob to simulate."});
+                return;
+            }
+            Entity fakeSpawned = matchingMob.get(0).newInstance(sender.getEntityWorld());
+            fakeSpawned.setPosition(sender.getPosition().getX(),sender.getPosition().getY(),sender.getPosition().getZ());
+            SpawnEventDetails fakeDetails = new SpawnEventDetails();
+            fakeDetails.fromSpawner=false;
+            fakeDetails.spawnEvent=null;
+            fakeDetails.entity = (EntityLiving) fakeSpawned;
+            Map<String, Integer> difficultyDetails = Maps.newHashMap();
+            try {
+                difficultyDetails.putAll(senderRegion.determineDifficultyForFakedSpawnEvent(fakeDetails));
+            }catch(Exception e){
+                sendChat(sender, new String[]{
+                        "There was a problem simulating difficulty.",
+                        "Please check the server log for details.",
+                        });
+                LOG.error(e.getMessage());
+                LOG.error(ExceptionUtils.getStackTrace(e));
+                return;
+            }
+            int totalDifficulty = 0;
+            for(String key : difficultyDetails.keySet()){
+                int thisControlDif = difficultyDetails.get(key);
+                totalDifficulty+=thisControlDif;
+                if(detailed){
+                    sendChat(sender, new String[]{key+": "+thisControlDif+" points."});
+                }
+            }
+            sendChat(sender, new String[]{totalDifficulty+" difficulty points"});
         }else if(args[0].equalsIgnoreCase("killmodified")){
             sendChat(sender, new String[]{"Killing all modified mobs in this dimension."});
             MobNBTHandler.getModifiedEntities(sender.getEntityWorld()).stream().forEach(mob->mob.setDead());
@@ -84,10 +135,26 @@ public class ServerCommand extends CommandBase {
                     "sync",
                     "killmodified",
                     "killmobs",
-                    "region"
+                    "region",
+                    "simulate",
+                    "simulateDetailed"
             };
             return CommandBase.getListOfStringsMatchingLastWord(args, validCompletions);
         }
+        if(args.length == 2 && args[0].startsWith("simulate")){
+            String[] validCompletions = getMobList();
+            return CommandBase.getListOfStringsMatchingLastWord(args, validCompletions);
+        }
         return CommandBase.getListOfStringsMatchingLastWord(args, new String[0]);
+    }
+
+    private String[] getMobList(){
+        List<String> lines = Lists.newArrayList();
+        for(EntityEntry entry : ForgeRegistries.ENTITIES.getValues()){
+            if(EntityLiving.class.isAssignableFrom(entry.getEntityClass())) {
+                lines.add(entry.getName());
+            }
+        }
+        return lines.toArray(new String[lines.size()]);
     }
 }
